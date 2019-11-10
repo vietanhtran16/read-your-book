3fce961c1bf16f1465dccf7e34c22cc2f95aa3b6
Jonathan Lindvall <jonathan.lindvall@fairfaxmedia.co.nz>
2019-11-11 10:34:00 +1300

    Generate Spring Boot application

---
+++ src/main/java/nz/co/stuff/planyourtrip/PlanYourTripApplication.java
@@ -0,0 +1,13 @@
+package nz.co.stuff.planyourtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanYourTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanYourTripApplication.class, args);
+	}
+
+}
