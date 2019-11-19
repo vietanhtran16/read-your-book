cb24a07fb2a5a64c769d1b0ccbe88b0975959e74
Viet Anh Tran <tranvietanh1996@gmail.com>
2019-11-19 21:28:19 +1300

    Set up junit

---
+++ src/test/java/nz/co/stuff/planyourtrip/persistence/InMemoryTripRepositoryTest.java
@@ -0,0 +1,12 @@
+package nz.co.stuff.planyourtrip.persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class InMemoryTripRepositoryTest {
+    @Test
+    void addition() {
+        assertEquals(2, 1 + 1);
+    }
+}
