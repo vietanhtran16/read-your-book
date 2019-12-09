5b53b9dd4b02e7b3607764023c57371d91d6498b
Viet Anh Tran <tranvietanh1996@gmail.com>
2019-12-09 21:46:52 +1300

    Remove plan your trip related code

--- src/main/java/nz/co/stuff/planyourtrip/persistence/TripRepository.java
+++
@@ -1,9 +0,0 @@
-package nz.co.stuff.planyourtrip.persistence;
-
-import nz.co.stuff.planyourtrip.domain.Trip;
-import java.util.List;
-
-public interface TripRepository {
-    List<Trip> getAllTrips();
-    Trip getTripById(long id);
-}
