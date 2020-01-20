2676fdb4fddb099a550247ccdb6683e94cae798d
Viet Anh Tran <viet.tran@fairfaxmedia.co.nz>
2020-01-21 09:31:26 +1300

    Remove create table since they are created automatically

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,3 @@
-DROP TABLE IF EXISTS Book;
-
-CREATE TABLE Book (
-  Id INT AUTO_INCREMENT PRIMARY KEY,
-  Name VARCHAR(250) NOT NULL,
-  Category VARCHAR(250) NOT NULL
-);
-
 INSERT INTO Book (Name, Category) VALUES
   ('Clean code', 'Software Development'),
   ('Clean architecture', 'Software Development'),
